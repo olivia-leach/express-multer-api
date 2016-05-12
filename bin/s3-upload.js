700cb4f388fe1c3ff0ad26c1cdb2bd4ddb249a00
olivia-leach <olivia.i.m.leach@gmail.com>
2016-05-12 15:29:14 -0400

    Move S3 specifics to separate file
    Refactor AWS only code into lib/aws-s3-upload.js
    Leave cli upload code in bin/s3-upload.js

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -7,10 +7,8 @@ require('dotenv').config();
 const fs = require('fs');
 // https://www.npmjs.com/package/file-type
 const fileType = require('file-type');
-const AWS = require('aws-sdk');
-const crypto = require('crypto');
 
-const s3 = new AWS.S3();
+const awsS3Upload = require('../lib/aws-s3-upload.js');
 
 const mimeType = (data) =>
   Object.assign({
@@ -35,45 +33,6 @@ const readFile = (filename) =>
     });
   });
 
-  const randomHexString = (length) =>
-  // use crypto to generate a random string for key
-  new Promise((resolve, reject) => {
-    crypto.randomBytes(length, (err, buf) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(buf.toString('hex'));
-      }
-    });
-  });
-
-const awsUpload = (file) =>
-
-  randomHexString(16)
-  .then((filename) => {
-    // assign directory to today's date
-    let dir = new Date().toISOString().split('T')[0];
-
-    // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#upload-property
-    return {
-      ACL: 'public-read',
-      Body: file.data,
-      Bucket: process.env.AWS_S3_BUCKET_NAME,
-      ContentType: file.mime,
-      Key: `${dir}/${filename}.${file.ext}`, // path that gets set up by S3
-    };
-  }).then(params =>
-    new Promise((resolve, reject) => {
-         s3.upload(params, (err, data) => {
-           if (err) {
-             reject(err);
-           } else {
-             resolve(data);
-           }
-         });
-       })
-     );
-
 // readFile() returns a promise
 readFile(filename)
   .then((data) => {
@@ -81,6 +40,6 @@ readFile(filename)
     file.data = data;
     return file;
   })
-  .then(awsUpload)
+  .then(awsS3Upload)
   .then((s3response) => console.log(s3response))
   .catch( console.error );
