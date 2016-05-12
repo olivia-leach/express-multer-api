27514aa6f6b068f26c506d350228ba657f1fe513
olivia-leach <olivia.i.m.leach@gmail.com>
2016-05-12 14:59:53 -0400

    Complete command line upload to AWS
    Generate low collision key for uploaded files

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,14 +1,22 @@
 'use strict';
 
+// loads the .env file int the environment
+require('dotenv').config();
+
 // require node modules
 const fs = require('fs');
 // https://www.npmjs.com/package/file-type
 const fileType = require('file-type');
+const AWS = require('aws-sdk');
+const crypto = require('crypto');
+
+const s3 = new AWS.S3();
 
 const mimeType = (data) =>
   Object.assign({
     ext: 'bin',
     mime: 'application/octet-stream',
+    // most generic content type. data shouldn't be interpreted as anything (image, etc.)
 }, fileType(data));
 
 // get filename from command line or assign it as an empty string
@@ -27,19 +35,44 @@ const readFile = (filename) =>
     });
   });
 
-const awsUpload = (file) => {
-  // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#upload-property
-  const options = {
-    ACL: 'public-read',
-    Body: file.data,
-    Bucket: 'olliiviia',
-    ContentType: file.mime,
-    // most generic content type. data shouldn't be interpreted as anything (image, etc.)
-    Key: `test/test.${file.ext}`, // path that gets set up by S3
-  };
-  // Promise.resolve creates a new promise that is resolved with whatever you pass to it
-  return Promise.resolve(options);
-};
+  const randomHexString = (length) =>
+  // use crypto to generate a random string for key
+  new Promise((resolve, reject) => {
+    crypto.randomBytes(length, (err, buf) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(buf.toString('hex'));
+      }
+    });
+  });
+
+const awsUpload = (file) =>
+
+  randomHexString(16)
+  .then((filename) => {
+    // assign directory to today's date
+    let dir = new Date().toISOString().split('T')[0];
+
+    // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#upload-property
+    return {
+      ACL: 'public-read',
+      Body: file.data,
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      ContentType: file.mime,
+      Key: `${dir}/${filename}.${file.ext}`, // path that gets set up by S3
+    };
+  }).then(params =>
+    new Promise((resolve, reject) => {
+         s3.upload(params, (err, data) => {
+           if (err) {
+             reject(err);
+           } else {
+             resolve(data);
+           }
+         });
+       })
+     );
 
 // readFile() returns a promise
 readFile(filename)
@@ -49,5 +82,5 @@ readFile(filename)
     return file;
   })
   .then(awsUpload)
-  .then((options) => console.log(options))
+  .then((s3response) => console.log(s3response))
   .catch( console.error );
