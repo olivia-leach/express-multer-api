ed5a5950baa579a61fac8e2278429240f9cf3a7b
olivia-leach <olivia.i.m.leach@gmail.com>
2016-05-12 13:27:26 -0400

    Read file/get type
    Use file-type to determine content type for s3 upload

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,7 +1,15 @@
 'use strict';
 
-// require node fs module
+// require node modules
 const fs = require('fs');
+// https://www.npmjs.com/package/file-type
+const fileType = require('file-type');
+
+const mimeType = (data) =>
+  Object.assign({
+    ext: 'bin',
+    mime: 'application/octet-stream',
+}, fileType(data));
 
 // get filename from command line or assign it as an empty string
 // empty string avoids fs.readFile blowing up
@@ -19,7 +27,27 @@ const readFile = (filename) =>
     });
   });
 
+const awsUpload = (file) => {
+  // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#upload-property
+  const options = {
+    ACL: 'public-read',
+    Body: file.data,
+    Bucket: 'olliiviia',
+    ContentType: file.mime,
+    // most generic content type. data shouldn't be interpreted as anything (image, etc.)
+    Key: `test/test.${file.ext}`, // path that gets set up by S3
+  };
+  // Promise.resolve creates a new promise that is resolved with whatever you pass to it
+  return Promise.resolve(options);
+};
+
 // readFile() returns a promise
 readFile(filename)
-  .then( (data) => console.log(`${filename} is ${data.length} bytes long.`))
+  .then((data) => {
+    let file = mimeType(data);
+    file.data = data;
+    return file;
+  })
+  .then(awsUpload)
+  .then((options) => console.log(options))
   .catch( console.error );
